cd21a86aa808ffd0e06cd15ab235d97bf160baf0
JeremmySm <je.sanmartinm@duocuc.cl>
2021-06-22 03:40:03 -0400

    Configuracion del admin/supersusuario

--- Exp3_Celis_SanMartin/exp3/admin.py
+++ Exp3_Celis_SanMartin/exp3/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from  .models import Usuario, Usuariomoroso, Servicio
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Usuariomoroso)
+admin.site.register(Servicio)
